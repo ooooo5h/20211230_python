1eef6b903969049d107629887f037adf3bc359a8
ooooo5h <ooooo5h91@gmail.com>
2021-12-30 11:02:30 +0900

    가입 완료 안내문구 추가하고, 2초간 정지

--- phone_book.py
+++ phone_book.py
@@ -1,5 +1,5 @@
 import pymysql
-from pymysql.cursors import Cursor
+from time import sleep
 # DB 연결자체는 main.py에서 설정
 # 여기서는 만들어진 연결정보를 받아서 사용하자
 
@@ -39,3 +39,6 @@ def sign_up():
     
     cursor.execute(sql)
     db_connect.commit()
+    
+    print('회원가입이 완료되었습니다. 메인화면으로 돌아갑니다.')
+    sleep(2)
